921a0c0b28e1922d01e71a89367d58757a07fc1d
joluna198 <95329940+joluna198@users.noreply.github.com>
2022-04-16 14:38:58 -0700

    Add files via upload

---
+++ coolappha/src/FireBase/Fire.js
@@ -0,0 +1,18 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBtWCVfXCAu3tSVypDyy7gTXe90BivqP6I",
+
+    authDomain: "coolapp-bffde.firebaseapp.com",
+
+    projectId: "coolapp-bffde",
+
+    storageBucket: "coolapp-bffde.appspot.com",
+
+    messagingSenderId: "174649673687",
+
+    appId: "1:174649673687:web:7e370c7d683a45902ced92"
+
+};
+
+const app = initializeApp(firebaseConfig);
