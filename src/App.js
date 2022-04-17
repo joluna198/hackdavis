a4bb0d2cb6b5a6ab01775fbabaf12b21892c4fe2
Shelby Sakamoto <megashelby@gmail.com>
2022-04-16 22:09:20 -0700

    stuff

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
-import {tasks} from './FireBase/userdata'
+import {tasks} from './FireBase/userdata' // EDIT: rename to CheckListDataFull?!
 import {useState} from "react";
 import Example from "./navBar"
-import CheckList from "./Checklist";
+import CheckList from "./CheckList";
 
 function App() {
     const [score, updateScore] = useState(0);
@@ -11,50 +11,52 @@ function App() {
     const addScore = (x) => {
         updateScore((prevScore) => prevScore + x);
     }
+
     const removeScore = (x) => {
         updateScore((prevScore) => prevScore - x);
-    }
-
-    const board = (layer1) => {
-        return (
-            <div className='task-board'>
-                {/*The first layer of the data*/}
-                <h1>{layer1.title}</h1>
-                {/*Second layer*/}
-                {layer1.data.map((layer2) => (
-                    <div>
-                        <h2>{layer2.title}</h2>
-                        {/* Third Layer*/}
-                        {layer2.data.map((layer3) => (
-                            <div>
-                                <p>{layer3.title}</p>
-                                <p>{layer3.score}</p>
-                            </div>
-                        ))}
-                    </div>
-
-                ))}
-            </div>
-        )
     }
 
     /// This function is for destructoring the data
     const boardElements = () => {
         return (
             <div className='task-board-container'>
-                {tasks.map((layer1) => (
-                    // board(layer1)
-                    <CheckList/>
+                {tasks.map((CheckListData) => (
+                    // board(CheckListData)
+                    <CheckList taskData={CheckListData}/>
                 ))}
             </div>
         )
     }
 
-
     return (
         <Example dashboard={boardElements()}/>
-
     );
 }
 
 export default App;
+
+
+
+// DEPRECIATED
+// const board = (layer1) => {
+//     return (
+//         <div className='task-board'>
+//             {/*The first layer of the data*/}
+//             <h1>{layer1.title}</h1>
+//             {/*Second layer*/}
+//             {layer1.data.map((layer2) => (
+//                 <div>
+//                     <h2>{layer2.title}</h2>
+//                     {/* Third Layer*/}
+//                     {layer2.data.map((layer3) => (
+//                         <div>
+//                             <p>{layer3.title}</p>
+//                             <p>{layer3.score}</p>
+//                         </div>
+//                     ))}
+//                 </div>
+
+//             ))}
+//         </div>
+//     )
+// }
